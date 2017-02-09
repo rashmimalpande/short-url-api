183fd3781bfd495dea6f2148e76aeeb15c7c4ee4
Rashmi Malpande <rashmimalpande@gmail.com>
2017-02-09 09:14:07 +0000

    Redirect to a given short url

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
 var code = 2214;
-
+var originalUrl;
 app.use('/new', function(req,res){
     var url = req.path;
-    var originalUrl = url.toString().slice(url.indexOf('/')+1);
+    originalUrl = url.toString().slice(url.indexOf('/')+1);
     
     res.json({'original' : originalUrl, 'short-url': code});
 });
 
 app.get('/:code', function(req, res){
-    
+    if(req.params.code == code.toString()){
+        res.redirect(originalUrl);
+    }
 });
 app.listen(process.env.PORT || 8080);
